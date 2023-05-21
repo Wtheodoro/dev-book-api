f1fd46365bdd028054006edee7ba476cac444218
Walison Theodoro <theodoro.walison@gmail.com>
2023-05-21 18:53:38 -0300

    ✨ | add users routes inside route with dummy data

--- src/router/routes/routes.go
+++ src/router/routes/routes.go
@@ -1,10 +1,25 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 type Route struct {
 	URI string
 	Method string
 	Function func(http.ResponseWriter, *http.Request)
 	RequiresAuthentication bool
+}
+
+// function Configure insert all routes inside router
+func Configure(r *mux.Router) *mux.Router {
+	routes := usersRoutes
+
+	for _, route := range routes {
+		r.HandleFunc(route.URI, route.Function).Methods(route.Method)
+	}
+
+	return r
 }
