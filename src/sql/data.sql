195537ce31923b6b4ca7f8136bed07aa2abdb78c
Walison Theodoro <theodoro.walison@gmail.com>
2023-06-16 17:40:23 -0300

    🔧 | sql queries

---
+++ src/sql/data.sql
@@ -0,0 +1,11 @@
+insert into users (name, nick, email, password)
+VALUES
+("Michael Jordan", "MJ", "michaeljordan@email.com", "$2a$10$q8G9BFMue67BziowL5.yqOjltcqhiA/Dasn7VR5gIb1hUL21N1Y2i"),
+("Oliver Sykes", "BMTH", "oliversykes@email.com", "$2a$10$q8G9BFMue67BziowL5.yqOjltcqhiA/Dasn7VR5gIb1hUL21N1Y2i"),
+("Kendrick Lamar", "be humble", "kendriclamar@email.com", "$2a$10$q8G9BFMue67BziowL5.yqOjltcqhiA/Dasn7VR5gIb1hUL21N1Y2i");
+
+insert into followers (user_id, follower_id)
+VALUES
+(1, 2),
+(3, 1),
+(1, 3);
--- src/sql/sql.sql
+++ src/sql/sql.sql
@@ -10,3 +10,18 @@ CREATE TABLE users(
   password VARCHAR(100) NOT NULL,
   created_at TIMESTAMP DEFAULT current_timestamp()
 ) ENGINE=INNODB;
+
+DROP TABLE IF EXISTS followers;
+CREATE TABLE followers(
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id)
+  REFERENCES users(id)
+  ON DELETE CASCADE,
+
+  follower_id INT NOT NULL,
+  FOREIGN KEY (follower_id)
+  REFERENCES users(id)
+  ON DELETE CASCADE,
+
+  PRIMARY KEY(user_id, follower_id)
+) ENGINE=INNODB;
